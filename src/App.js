7c094afc5c7760dd402d4886af215e39ed6d924c
charankk54-star <charankk54@gmail.com>
2026-07-04 23:23:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
+import { Descope, useSession } from "@descope/react-sdk";
 import { useEffect, useState } from "react";
 
 function App() {
-	const [data, setData] = useState([]);
+  const { isAuthenticated, isSessionLoading } = useSession();
+  const [data, setData] = useState(null);
 
-	useEffect(() => {
-		fetch("https://descope-escape-room.com/api/data", {
-			method: "POST",
-			headers: {
-				Accept: "application/json, text/plain, */*",
-				"Content-Type": "application/json",
-			},
-		})
-			.then((response) => response.json())
-			.then((resJson) => {
-				setData(resJson);
-				console.log(resJson);
-				// data = resJson
-			});
-	}, []);
+  useEffect(() => {
+    if (!isAuthenticated) return;
 
-	return (
-		<div>
-			<div id="login"></div>
-			<code>{data.body}</code>
-		</div>
-	);
+    fetch("https://descope-escape-room.com/api/data", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch");
+        }
+        return response.json();
+      })
+      .then((resJson) => {
+        console.log(resJson);
+        setData(resJson);
+      })
+      .catch(console.error);
+  }, [isAuthenticated]);
+
+  return (
+    <div>
+      {!isAuthenticated && (
+        <Descope
+          flowId="sign-up-or-in"
+          theme="light"
+          onSuccess={(e) => console.log(e.detail.user)}
+          onError={(err) => console.log(err)}
+        />
+      )}
+
+      {isSessionLoading && <p>Loading...</p>}
+
+      {!isSessionLoading && isAuthenticated && (
+        <div>
+          <div id="login"></div>
+          <code>{data?.body}</code>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
