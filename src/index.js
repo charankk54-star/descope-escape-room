effadeeafb24aa170d467b4180732c8a47a5a0e0
charankk54-star <charankk54@gmail.com>
2026-07-04 23:22:39 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { AuthProvider } from '@descope/react-sdk';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider projectId="P3Fy0n5N7J3q9RxvP1W8eCJ4jgRY">
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
